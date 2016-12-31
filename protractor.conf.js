a4a3122cb62272d25251f9a2ce39f10d647bb98d
kecheon <kecheon@gmail.com>
2017-01-01 00:44:04 +0900

    e2e test ok

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['src/test/test.spec.e2e.js'],
+  useAllAngular2AppRoots: true,
   onPrepare: function() {
     // Override the timeout for webdriver.
     browser.manage().timeouts().setScriptTimeout(10000);
