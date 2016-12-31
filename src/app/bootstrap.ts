95d1985e034c29f41b0f6d3f14219be5e3fe4d0f
kecheon <kecheon@gmail.com>
2016-12-31 13:44:13 +0900

    add comments

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
 import {UserService} from './user-service';
 import {AppComponent} from './app-component';
 import {LoginService} from './login-service';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {GreetingComponent} from './greeting-component';
 import {BorderComponent} from './border-component';
 
+// to manage every dependencies of MyAppModule at once.
+// we don't need to declare directives or providers dependent within AppComponent 
 @NgModule({
   declarations: [AppComponent, GreetingComponent, BorderComponent],
   providers: [LoginService, UserService],
